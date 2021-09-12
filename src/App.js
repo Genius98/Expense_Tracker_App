43fffb52ffebf8a06485c578d7dbe945783012e0
GeniusJamir <geniusjamir98@gmail.com>
2021-09-12 23:50:39 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -3,17 +3,7 @@ import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
-const DUMMY_EXPENSES = [
-  
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-  {
-    id: "e3",
-    title: "Car Insurance",
-    amount: 294.67,
-    date: new Date(2021, 2, 28),
-  },
-  
-];
+const DUMMY_EXPENSES = [];
 
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
